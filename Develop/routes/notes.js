289e6c06d458339011e83dc3cff813c022dbdccd
Cong Wang <wangcong525@hotmail.com>
2024-08-12 15:08:18 -0400

    create and organize routes

---
+++ Develop/routes/notes.js
@@ -0,0 +1,33 @@
+const notes = require('express').Router();
+const uuid = require('../helpers/uuid');
+
+// Helper functions for reading and writing to the JSON file
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+// This API route is a GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// This API route is a POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+
+  const { title, text} = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+     title_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
---
+++ Develop/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const api = require('./routes/index');
+const notesRouter = require('./routes/notes');
+const PORT = 3001;
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static('public'));
+
+// Send all the requests that begin with /api to the index.js in the routes folder
+app.use('/api', api);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+  );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
